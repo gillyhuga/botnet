4535d2aacc4b5ddabd87d4150723e93fc6024cb7
Gilly <gilly.huga@yahoo.co.id>
2021-10-26 02:14:56 +0700

    add modal kegiatan

--- public/js/app.js
+++ public/js/app.js
@@ -3100,25 +3100,6 @@ function within(min, value, max) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var bootstrap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js");
 
-var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'));
-triggerTabList.forEach(function (triggerEl) {
-  var tabTrigger = new bootstrap.Tab(triggerEl);
-  triggerEl.addEventListener('click', function (event) {
-    event.preventDefault();
-    tabTrigger.show();
-  });
-});
-var triggerEl = document.querySelector('#myTab a[href="#profile"]');
-bootstrap.Tab.getInstance(triggerEl).show(); // Select tab by name
-
-var triggerFirstTabEl = document.querySelector('#myTab li:first-child a');
-bootstrap.Tab.getInstance(triggerFirstTabEl).show(); // Select first tab
-
-$("#pop").on("click", function () {
-  $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
-
-  $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
-});
 
 /***/ }),
 
