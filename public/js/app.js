140385780be68fb26810666a9c32f8621b9d8fb8
Gilly <gilly.huga@yahoo.co.id>
2021-10-26 01:41:43 +0700

    fix route js

--- public/js/app.js
+++ public/js/app.js
@@ -3114,6 +3114,12 @@ bootstrap.Tab.getInstance(triggerEl).show(); // Select tab by name
 var triggerFirstTabEl = document.querySelector('#myTab li:first-child a');
 bootstrap.Tab.getInstance(triggerFirstTabEl).show(); // Select first tab
 
+$("#pop").on("click", function () {
+  $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+
+  $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
+
 /***/ }),
 
 /***/ "./node_modules/bootstrap/dist/js/bootstrap.esm.js":
